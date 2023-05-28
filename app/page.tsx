13f1f4b2d3babc8deac1b04ab3cfcf0cc0a2f312
ercumentlacin <forever.ducale@gmail.com>
2023-05-28 17:48:50 +0300

    refactor(page.tsx): update homepage content with more detailed information about the developer's skills and interests

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,7 @@ export default function HomePage() {
       <main className="container max-w-2xl mx-auto my-5">
         <h1 className="home-title">Ercüment Laçın</h1>
 
-        <p className="my-5">
-          Hey, I&apos;m Ercüment. I&apos;m a software engineer and a full-stack
-          developer.
-        </p>
-
-        <div className="flex gap-5">
+        <div className="flex flex-col gap-5 sm:flex-row">
           <Image
             src="/images/avatar.jpg"
             alt="me"
@@ -21,12 +16,26 @@ export default function HomePage() {
             height="96"
             className="object-cover object-top w-24 h-24 rounded-full aspect-square grayscale"
           />
-          <p>
-            A qualified frontend developer with 6 years experienced with
-            excellent skills in analyzing and modifying existing software, as
-            well as developing, designing, and testing end-user applications
-            that match user needs – all using software programming languages.
-          </p>
+          <section className="space-y-3">
+            <p>
+              Hey, I&apos;m a frontend developer with a passion for creating
+              engaging and user-friendly web experiences. With 4 years of
+              professional experience, I have developed a strong skill set in
+              JavaScript, TypeScript, and React. I also have knowledge of
+              Node.js and have experience in developing full stack applications
+              using Next.js.
+            </p>
+            <p>
+              My goal is to bring ideas to life through clean and elegant code.
+              I believe in the power of collaboration and enjoy working in teams
+              to deliver high-quality projects.
+            </p>
+            <p>
+              When I&apos;m not coding, you can find me exploring the world of
+              anime, staying active through sports, and expressing my creativity
+              through graphite drawings.
+            </p>
+          </section>
         </div>
       </main>
     </WithTransition>
